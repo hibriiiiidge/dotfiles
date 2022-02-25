7d8e890188128ed26fc87eebbb9889bf4fe7b2e3
yamasakih <yamasaki.phone@gmail.com>
2022-02-25 22:09:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# .zshrc
 cd ~/
 ln -fs ~/dotfiles/.zshrc .
 
+# powerline_shell
 pip install powerline_shell
+
+# z
+git clone git@github.com:rupa/z.git ~/z
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
+# git completions
+mkdir -p ~/.zsh/completions
+cd ~/.zsh/completions
+wget https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
+wget -O _git https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.zsh
