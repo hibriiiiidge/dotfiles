b06bab5e1afbfe9a262681888d312f45db233d4c
yamasakih <yamasaki.phone@gmail.com>
2022-04-05 23:37:33 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,11 +19,6 @@ pip install powerline_shell
 # z
 git clone git@github.com:rupa/z.git ~/z
 
-# fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-yes | ~/.fzf/install
-apt install locales-all
-
 # git completions
 mkdir -p ~/.zsh/completions
 cd ~/.zsh/completions
@@ -32,3 +27,16 @@ wget -O _git https://raw.githubusercontent.com/git/git/master/contrib/completion
 
 # zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+
+# fzf
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+yes | ~/.fzf/install
+
+# install gh
+gh -version &> /dev/null
+if [ $? -ne 0 ] ; then
+  curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | gpg --no-tty --dearmor -o /usr/share/keyrings/githubcli-archive-keyring.gpg \
+    && echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | tee /etc/apt/sources.list.d/github-cli.list > /dev/null \
+    && apt update \
+    && apt install -y --no-install-recommends gh
+fi
